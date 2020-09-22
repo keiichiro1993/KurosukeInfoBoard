d8de9824881141d82873725b1fcfccf9ce875068
Keiichiro Wakasa <t.i.docomox-3-6x@hotmail.co.jp>
2020-09-23 00:59:57 +0900

    enabled calendar ink save

--- Controls/CalendarControl.xaml.cs
+++ Controls/CalendarControl.xaml.cs
@@ -1,14 +1,18 @@
 ﻿using KurosukeInfoBoard.Models;
+using KurosukeInfoBoard.Utils;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Core;
 using Windows.UI.Input.Inking;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -41,6 +45,52 @@ namespace KurosukeInfoBoard.Controls
                 drawingAttributes.Color = Windows.UI.Colors.White;
                 calendarCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
             }
+
+            calendarCanvas.InkPresenter.StrokesCollected += InkPresenter_StrokesCollected;
+        }
+
+        private DateTime lastModifiedTime = DateTime.Now;
+        private async void InkPresenter_StrokesCollected(InkPresenter sender, InkStrokesCollectedEventArgs args)
+        {
+            lastModifiedTime = DateTime.Now;
+            await Task.Delay(2000);
+            if (DateTime.Now - lastModifiedTime > new TimeSpan(0, 0, 2))
+            {
+                //save only if more than 2 secs from last modification
+
+                Windows.Storage.StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+                try
+                {
+                    var file = await localFolder.CreateFileAsync(CalendarMonth.Month.ToString("yyyy-MM-dd") + ".png", Windows.Storage.CreationCollisionOption.ReplaceExisting);
+                    Windows.Storage.CachedFileManager.DeferUpdates(file);
+                    using (IRandomAccessStream stream = await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
+                    {
+                        using (IOutputStream outputStream = stream.GetOutputStreamAt(0))
+                        {
+                            await calendarCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream);
+                            await outputStream.FlushAsync();
+                        }
+                    }
+
+                    // Finalize write so other apps can update file.
+                    Windows.Storage.Provider.FileUpdateStatus status = await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+
+                    if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                    {
+                        DebugHelper.WriteDebugLog("Calendar ink saved.");
+                    }
+                    else
+                    {
+                        throw new Exception("Calendar ink couldn't be saved. Status=" + status.ToString() + ".");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DebugHelper.WriteErrorLog("Error occured while saving calendar canvas.", ex);
+                    await new MessageDialog(ex.Message, "Error occured while saving calendar canvas.").ShowAsync();
+                }
+            }
         }
 
         public CalendarMonth CalendarMonth
@@ -53,13 +103,36 @@ namespace KurosukeInfoBoard.Controls
           DependencyProperty.Register(nameof(CalendarMonth), typeof(CalendarMonth),
             typeof(CalendarControl), new PropertyMetadata(null, new PropertyChangedCallback(OnCalendarDayChanged)));
 
-        private static void OnCalendarDayChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private async static void OnCalendarDayChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             CalendarControl cc = d as CalendarControl;
             var month = (CalendarMonth)e.NewValue;
             for (var i = 0; i < month.CalendarDays.Count; i++)
             {
                 cc.itemControls[i].CalendarDay = month.CalendarDays[i];
+            }
+
+            Windows.Storage.StorageFolder localFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+
+            try
+            {
+                if (await localFolder.TryGetItemAsync(cc.CalendarMonth.Month.ToString("yyyy-MM-dd") + ".png") != null)
+                {
+                    var file = await localFolder.CreateFileAsync(cc.CalendarMonth.Month.ToString("yyyy-MM-dd") + ".png", Windows.Storage.CreationCollisionOption.OpenIfExists);
+
+                    using (IRandomAccessStream stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                    {
+                        using (var inputStream = stream.GetInputStreamAt(0))
+                        {
+                            await cc.calendarCanvas.InkPresenter.StrokeContainer.LoadAsync(inputStream);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteErrorLog("Error occured while loading calendar canvas.", ex);
+                await new MessageDialog(ex.Message, "Error occured while loading calendar canvas.").ShowAsync();
             }
         }
 
