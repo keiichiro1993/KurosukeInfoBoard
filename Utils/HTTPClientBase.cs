982039df20061c05a84b65ba7ec58235f8d40c0b
Keiichiro Wakasa <t.i.docomox-3-6x@hotmail.co.jp>
2020-09-23 20:48:11 +0900

    add stopwatch

--- Utils/HTTPClientBase.cs
+++ Utils/HTTPClientBase.cs
@@ -1,6 +1,7 @@
 ﻿using KurosukeInfoBoard.Models.Auth;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -13,6 +14,9 @@ namespace KurosukeInfoBoard.Utils
         protected TokenBase token;
         protected async Task<string> GetAsync(string url, int retry = 1)
         {
+            DebugHelper.WriteDebugLog("Get Async called. Request URL=" + url + " Available retry=" + retry + ".");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.AccessToken);
@@ -22,7 +26,7 @@ namespace KurosukeInfoBoard.Utils
                 {
                     if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
                     {
-                        DebugHelper.WriteDebugLog("Get Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                        DebugHelper.WriteDebugLog("Get Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
                         return null;
                     }
                     else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
@@ -34,20 +38,23 @@ namespace KurosukeInfoBoard.Utils
                         }
                         else
                         {
-                            throw new UnauthorizedAccessException("Get Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                            throw new UnauthorizedAccessException("Get Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
                         }
                     }
 
-                    throw new HttpRequestException("Get Async failed.Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
+                    throw new HttpRequestException("Get Async failed.Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
                 }
 
-                DebugHelper.WriteDebugLog("Get Async succeeded. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                DebugHelper.WriteDebugLog("Get Async succeeded. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
                 return await response.Content.ReadAsStringAsync();
             }
         }
 
         protected async Task<string> PostAsync(string url, HttpContent content, int retry = 1)
         {
+            DebugHelper.WriteDebugLog("Post Async called. Request URL=" + url + " Available retry=" + retry + ".");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
             using (var client = new HttpClient())
             {
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.AccessToken);
@@ -64,14 +71,14 @@ namespace KurosukeInfoBoard.Utils
                         }
                         else
                         {
-                            throw new UnauthorizedAccessException("Post Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                            throw new UnauthorizedAccessException("Post Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
                         }
                     }
-                    DebugHelper.WriteDebugLog("HTTP request was not successful. Request URL=" + response.RequestMessage.Method + " " + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
-                    throw new HttpRequestException("HTTP request was not successful. Request URL=" + response.RequestMessage.Method + " " + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
+                    DebugHelper.WriteDebugLog("HTTP request was not successful. Request URL=" + response.RequestMessage.Method + " " + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
+                    throw new HttpRequestException("HTTP request was not successful. Request URL=" + response.RequestMessage.Method + " " + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
                 }
 
-                DebugHelper.WriteDebugLog("Post Async succeeded. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                DebugHelper.WriteDebugLog("Post Async succeeded. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ". Elapsed time=" + stopwatch.Elapsed.TotalSeconds + "secs.");
                 return await response.Content.ReadAsStringAsync();
             }
         }
