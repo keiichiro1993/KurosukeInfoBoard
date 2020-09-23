a9f26f1cfa192b07ebde75c3d167912f7a1db42f
Keiichiro Wakasa <t.i.docomox-3-6x@hotmail.co.jp>
2020-09-23 20:40:17 +0900

    enabled token refresh

--- Utils/HTTPClientBase.cs
+++ Utils/HTTPClientBase.cs
@@ -11,7 +11,7 @@ namespace KurosukeInfoBoard.Utils
     public class HTTPClientBase
     {
         protected TokenBase token;
-        protected async Task<string> GetAsync(string url)
+        protected async Task<string> GetAsync(string url, int retry = 1)
         {
             using (var client = new HttpClient())
             {
@@ -25,7 +25,20 @@ namespace KurosukeInfoBoard.Utils
                         DebugHelper.WriteDebugLog("Get Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
                         return null;
                     }
-                    throw new InvalidOperationException("HTTP request was not successful. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
+                    else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        if (retry > 0)
+                        {
+                            await token.AcquireNewToken();
+                            return await GetAsync(url, retry - 1);
+                        }
+                        else
+                        {
+                            throw new UnauthorizedAccessException("Get Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                        }
+                    }
+
+                    throw new HttpRequestException("Get Async failed.Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
                 }
 
                 DebugHelper.WriteDebugLog("Get Async succeeded. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
@@ -33,7 +46,7 @@ namespace KurosukeInfoBoard.Utils
             }
         }
 
-        protected async Task PostAsync(string url, HttpContent content)
+        protected async Task<string> PostAsync(string url, HttpContent content, int retry = 1)
         {
             using (var client = new HttpClient())
             {
@@ -42,11 +55,24 @@ namespace KurosukeInfoBoard.Utils
 
                 if (!response.IsSuccessStatusCode)
                 {
+                    if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        if (retry > 0)
+                        {
+                            await token.AcquireNewToken();
+                            return await PostAsync(url, content, retry - 1);
+                        }
+                        else
+                        {
+                            throw new UnauthorizedAccessException("Post Async failed. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                        }
+                    }
                     DebugHelper.WriteDebugLog("HTTP request was not successful. Request URL=" + response.RequestMessage.Method + " " + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
-                    throw new InvalidOperationException("HTTP request was not successful. Request URL=" + response.RequestMessage.Method + " " + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
+                    throw new HttpRequestException("HTTP request was not successful. Request URL=" + response.RequestMessage.Method + " " + response.RequestMessage.RequestUri.AbsoluteUri + ". HTTP Status=" + response.StatusCode + ". Reason=" + response.ReasonPhrase);
                 }
 
                 DebugHelper.WriteDebugLog("Post Async succeeded. Request URL=" + response.RequestMessage.RequestUri.AbsoluteUri + " HTTP Status=" + response.StatusCode + ".");
+                return await response.Content.ReadAsStringAsync();
             }
         }
     }
