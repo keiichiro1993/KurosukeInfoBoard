fef904407024914c5eaab0c27b11cca8aac07bf5
Keiichiro Wakasa <t.i.docomox-3-6x@hotmail.co.jp>
2020-10-20 23:56:24 +0900

    highlighting today and change colors of holidays

--- Controls/CalendarItemControl.xaml.cs
+++ Controls/CalendarItemControl.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using KurosukeInfoBoard.Models;
+using KurosukeInfoBoard.Models.Google;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -48,6 +52,32 @@ namespace KurosukeInfoBoard.Controls
             {
                 cc.DateTextBlock.Text = day.Date.Day.ToString();
                 cc.EventsListView.ItemsSource = day.Events;
+            }
+
+            //change day text color if it's holiday
+            switch (day.Date.DayOfWeek)
+            {
+                case DayOfWeek.Saturday:
+                    cc.DateTextBlock.Foreground = Utils.UIHelper.ColorConverter.HexToBrush("#FF4B75FF");
+                    break;
+                case DayOfWeek.Sunday:
+                    cc.DateTextBlock.Foreground = Utils.UIHelper.ColorConverter.HexToBrush("#FFE55F5F");
+                    break;
+                default:
+                    cc.DateTextBlock.Foreground = (SolidColorBrush)cc.Resources["DefaultTextForegroundThemeBrush"];
+                    break;
+            }
+
+            //change border color if today
+            if (day.Date.Day == DateTime.Now.Day)
+            {
+                cc.topBorder.BorderBrush = (SolidColorBrush)cc.Resources["DefaultTextForegroundThemeBrush"];
+                cc.topBorder.BorderThickness = new Thickness(4);
+            }
+            else
+            {
+                cc.topBorder.BorderBrush = new SolidColorBrush(Windows.UI.Colors.Gray);
+                cc.topBorder.BorderThickness = new Thickness(1);
             }
         }
 
---
+++ Utils/UIHelper/ColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace KurosukeInfoBoard.Utils.UIHelper
+{
+    public class ColorConverter
+    {
+        public static SolidColorBrush HexToBrush(string hex)
+        {
+            var a = (byte)System.Convert.ToUInt32(hex.Substring(1, 2), 16);
+            var r = (byte)System.Convert.ToUInt32(hex.Substring(3, 2), 16);
+            var g = (byte)System.Convert.ToUInt32(hex.Substring(5, 2), 16);
+            var b = (byte)System.Convert.ToUInt32(hex.Substring(7, 2), 16);
+            Color color = Color.FromArgb(255, r, g, b);
+            return new SolidColorBrush(color);
+        }
+    }
+}
