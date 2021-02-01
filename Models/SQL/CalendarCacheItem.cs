cff0a1456abe89a7e7409a0b96eb9cae1eae50d0
Keiichiro Wakasa <t.i.docomox-3-6x@hotmail.co.jp>
2021-02-02 00:24:45 +0900

    impl calendar item cache db entity/helper

---
+++ Models/SQL/CalendarCacheItem.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace KurosukeInfoBoard.Models.SQL
+{
+
+    [Table("CalendarCache")]
+    public class CalendarCacheEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ID { get; set; }
+
+        [Required]
+        public string CalendarId { get; set; }
+
+        [Required]
+        public string CalendarName { get; set; }
+
+        [Required]
+        public bool IsEnabled { get; set; }
+
+        [Required]
+        public string AccountType { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+    }
+
+    public class CalendarCacheContext : DbContext
+    {
+        private static string DBFileName = "calendarcache.db";
+
+        public DbSet<CalendarCacheEntity> CalendarCache { get; internal set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=" + ApplicationData.Current.LocalFolder.Path + "\\" + DBFileName);
+        }
+    }
+}
---
+++ Utils/DBHelpers/CalendarCacheHelper.cs
@@ -0,0 +1,97 @@
+﻿using KurosukeInfoBoard.Models.Common;
+using KurosukeInfoBoard.Models.SQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurosukeInfoBoard.Utils.DBHelpers
+{
+    public class CalendarCacheHelper
+    {
+        public async Task Init()
+        {
+            using (var context = new CalendarCacheContext())
+            {
+                await context.Database.EnsureCreatedAsync();
+            }
+        }
+
+        private CalendarCacheEntity ICalendarToEntity(CalendarBase calendar)
+        {
+            var entity = new CalendarCacheEntity();
+            entity.CalendarId = calendar.Id;
+            entity.CalendarName = calendar.Name;
+            entity.IsEnabled = calendar.IsEnabled;
+            entity.AccountType = calendar.AccountType;
+            entity.UserId = calendar.UserId;
+
+            return entity;
+        }
+
+        public async Task SaveCalendar(CalendarBase calendar)
+        {
+            var entity = ICalendarToEntity(calendar);
+            using (var context = new CalendarCacheContext())
+            {
+                await context.CalendarCache.AddAsync(entity);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// This method checks if the calendar item is enabled for display.
+        /// Add calendar entity to DB if the item doesn't exist.
+        /// </summary>
+        /// <param name="calendar">the calendar item to check</param>
+        /// <returns>True if it doens't exist on cache or exists and enabled. False if disabled.</returns>
+        public async Task<bool> CheckIfEnabled(CalendarBase calendar)
+        {
+            using (var context = new CalendarCacheContext())
+            {
+                var match = from item in context.CalendarCache
+                            where item.CalendarId == calendar.Id && item.UserId == calendar.UserId
+                            select item;
+                if (match.Any())
+                {
+                    return match.First().IsEnabled;
+                }
+                else
+                {
+                    calendar.IsEnabled = true;
+                    await SaveCalendar(calendar);
+                    return true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Add or Update calendar.
+        /// </summary>
+        /// <param name="calendar">The calaendar item to Add or Update</param>
+        /// <returns></returns>
+        public async Task AddUpdateCalendarCache(CalendarBase calendar)
+        {
+            using (var context = new CalendarCacheContext())
+            {
+                var match = from item in context.CalendarCache
+                            where item.CalendarId == calendar.Id && item.UserId == calendar.UserId
+                            select item;
+
+                if (match.Any())
+                {
+                    var entity = match.First();
+                    entity.IsEnabled = calendar.IsEnabled;
+                    context.Update(entity);
+
+                    await context.SaveChangesAsync();
+                    return;
+                }
+            }
+
+            await SaveCalendar(calendar);
+        }
+
+    }
+}
