7f87d83a678d43e304c111ba6709d2181101c7cc
Keiichiro Wakasa <keiichiro1993@gmail.com>
2021-05-23 22:39:41 +0900

    N/A

--- Controls/Calendar/CalendarItemControl.xaml.cs
+++ Controls/Calendar/CalendarItemControl.xaml.cs
@@ -1,22 +1,8 @@
 ﻿using KurosukeInfoBoard.Models;
-using KurosukeInfoBoard.Models.Google;
-using Microsoft.Toolkit.Uwp.UI.Extensions;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // ユーザー コントロールの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234236 を参照してください
 
